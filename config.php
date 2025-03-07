6ac0c70d6323e1e716f68af4e5c5e03ac4a39489
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-03-07 23:13:54 +0530

    favicon

--- config.php
+++ config.php
@@ -3,7 +3,6 @@
 return [
     'production' => false,
     'baseUrl' => 'https://smp-print.test',
-    'title' => '',
     'description' => 'Website description.',
     'seo_image' => '/assets/images/favicon.png',
     'collections' => [],
